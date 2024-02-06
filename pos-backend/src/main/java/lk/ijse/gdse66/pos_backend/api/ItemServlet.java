8227632de8c259e9d88c9e4eeb78cb4faa4464d4
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2024-02-07 03:05:08 +0530

    item post method updated

--- pos-backend/src/main/java/lk/ijse/gdse66/pos_backend/api/ItemServlet.java
+++ pos-backend/src/main/java/lk/ijse/gdse66/pos_backend/api/ItemServlet.java
@@ -9,7 +9,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lk.ijse.gdse66.pos_backend.bo.BoFactory;
 import lk.ijse.gdse66.pos_backend.bo.custom.ItemBO;
-import lk.ijse.gdse66.pos_backend.dto.CustomerDTO;
 import lk.ijse.gdse66.pos_backend.dto.ItemDTO;
 
 import java.io.IOException;
@@ -32,4 +31,49 @@ public class ItemServlet extends HttpServlet {
             throwables.printStackTrace();
         }
     }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Jsonb jsonb = JsonbBuilder.create();
+        ItemDTO itemDTO = jsonb.fromJson(req.getReader(), ItemDTO.class);
+        String code = itemDTO.getCode();
+        String description = itemDTO.getDescription();
+        double unitPrice = itemDTO.getUnitPrice();
+        int qty = itemDTO.getQtyOnHand();
+
+
+        if(code==null || !code.matches("I\\d{3}")){
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Item Code is empty or invalid");
+            return;
+        } else if (description == null || !description.matches("[A-Za-z ]+")) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Description is empty or invalid");
+            return;
+        } else if (unitPrice < 0.0) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Unit Price is empty or invalid");
+            return;
+        }else if (qty < 0 ){
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Quantity is empty or invalid");
+            return;
+        }
+
+        try {
+
+            boolean saveItem = itemBO.saveItem(new ItemDTO(code,description,unitPrice,qty));
+            if (saveItem) {
+                resp.setStatus(HttpServletResponse.SC_CREATED);
+                resp.getWriter().write("Added item successfully");
+            }else {
+                resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Failed to save the item");
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+
 }
