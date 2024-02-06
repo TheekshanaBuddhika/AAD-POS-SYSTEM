42f9deaa53c084210a9d2c2c5ba4e4a8b6342834
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2024-02-06 14:55:15 +0530

    context xml added

--- pos-backend/src/main/java/lk/ijse/gdse66/pos_backend/api/CustomerServlet.java
+++ pos-backend/src/main/java/lk/ijse/gdse66/pos_backend/api/CustomerServlet.java
@@ -14,5 +14,8 @@ public class CustomerServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+
+
+
     }
 }
