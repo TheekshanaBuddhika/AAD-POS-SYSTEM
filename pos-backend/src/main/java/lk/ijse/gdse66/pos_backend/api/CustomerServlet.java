b940e96a7d9dca1052ae17fda4f0d322b4a0c587
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2024-02-06 09:29:17 +0530

    customer updated

--- pos-backend/src/main/java/lk/ijse/gdse66/pos_backend/api/CustomerServlet.java
+++ pos-backend/src/main/java/lk/ijse/gdse66/pos_backend/api/CustomerServlet.java
@@ -1,16 +1,18 @@
 package lk.ijse.gdse66.pos_backend.api;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
+@WebServlet(name = "Customer", urlPatterns = "/customers", loadOnStartup = 1)
 public class CustomerServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+
     }
 }
