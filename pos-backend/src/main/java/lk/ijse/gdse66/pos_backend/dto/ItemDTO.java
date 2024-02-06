a5f36edb415f84e6381222815c64695f91ced057
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2024-02-07 02:34:47 +0530

    item dto created

---
+++ pos-backend/src/main/java/lk/ijse/gdse66/pos_backend/dto/ItemDTO.java
@@ -0,0 +1,60 @@
+package lk.ijse.gdse66.pos_backend.dto;
+
+public class ItemDTO {
+    private String code;
+    private String description;
+    private int qtyOnHand;
+    private double unitPrice;
+
+    @Override
+    public String toString() {
+        return "ItemDTO{" +
+                "code='" + code + '\'' +
+                ", description='" + description + '\'' +
+                ", qtyOnHand=" + qtyOnHand +
+                ", unitPrice=" + unitPrice +
+                '}';
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQtyOnHand() {
+        return qtyOnHand;
+    }
+
+    public void setQtyOnHand(int qtyOnHand) {
+        this.qtyOnHand = qtyOnHand;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public ItemDTO() {
+    }
+
+    public ItemDTO(String code, String description, int qtyOnHand, double unitPrice) {
+        this.code = code;
+        this.description = description;
+        this.qtyOnHand = qtyOnHand;
+        this.unitPrice = unitPrice;
+    }
+}
