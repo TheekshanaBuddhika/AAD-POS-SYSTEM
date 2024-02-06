a22f01f46ba38e01bee7b66e3ba52cabe65fd1a2
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2024-02-06 22:05:36 +0530

    dao layer added

--- pos-backend/src/main/java/lk/ijse/gdse66/pos_backend/bo/custom/impl/CustomerBOImpl.java
+++ pos-backend/src/main/java/lk/ijse/gdse66/pos_backend/bo/custom/impl/CustomerBOImpl.java
@@ -2,6 +2,7 @@ package lk.ijse.gdse66.pos_backend.bo.custom.impl;
 
 import lk.ijse.gdse66.pos_backend.bo.custom.CustomerBO;
 import lk.ijse.gdse66.pos_backend.dto.CustomerDTO;
+import lk.ijse.gdse66.pos_backend.entity.Customer;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -9,6 +10,11 @@ import java.util.ArrayList;
 public class CustomerBOImpl implements CustomerBO {
     @Override
     public ArrayList<CustomerDTO> getAllCustomers() throws SQLException, ClassNotFoundException {
-        return null;
+        ArrayList<Customer> all = customerDAO.getAll();
+        ArrayList<CustomerDTO> arrayList= new ArrayList<>();
+        for (Customer c : all) {
+            arrayList.add(new CustomerDTO(c.getId(),c.getName(),c.getAddress(),c.getSalary()));
+        }
+        return arrayList;
     }
 }
