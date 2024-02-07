408af9b79e49fc5190d0797070bdb0245ad8e3d1
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2024-02-07 15:01:06 +0530

    entity updated

--- pos-backend/src/main/java/lk/ijse/gdse66/pos_backend/entity/Item.java
+++ pos-backend/src/main/java/lk/ijse/gdse66/pos_backend/entity/Item.java
@@ -1,51 +1,15 @@
 package lk.ijse.gdse66.pos_backend.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Item {
     private String code;
     private String description;
     private double unitPrice;
     private int qtyOnHand;
-
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public int getQtyOnHand() {
-        return qtyOnHand;
-    }
-
-    public void setQtyOnHand(int qtyOnHand) {
-        this.qtyOnHand = qtyOnHand;
-    }
-
-    public double getUnitPrice() {
-        return unitPrice;
-    }
-
-    public void setUnitPrice(double unitPrice) {
-        this.unitPrice = unitPrice;
-    }
-
-    public Item(String code, String description, double unitPrice, int qtyOnHand) {
-        this.code = code;
-        this.description = description;
-        this.unitPrice = unitPrice;
-        this.qtyOnHand = qtyOnHand;
-    }
-
-    public Item() {
-    }
 }
