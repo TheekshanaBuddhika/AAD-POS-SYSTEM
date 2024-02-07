f8cc1a2fbc6a0ffea764db194f74bce65fd39b75
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2024-02-07 15:02:15 +0530

    dto updated

--- pos-backend/src/main/java/lk/ijse/gdse66/pos_backend/dto/CustomerDTO.java
+++ pos-backend/src/main/java/lk/ijse/gdse66/pos_backend/dto/CustomerDTO.java
@@ -1,61 +1,18 @@
 package lk.ijse.gdse66.pos_backend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class CustomerDTO {
 
     private String id;
     private String name;
     private String address;
     private double salary;
-
-    @Override
-    public String toString() {
-        return "CustomerDTO{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                ", address='" + address + '\'' +
-                ", salary=" + salary +
-                '}';
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
-
-    public CustomerDTO() {
-    }
-
-    public CustomerDTO(String id, String name, String address, double salary) {
-        this.id = id;
-        this.name = name;
-        this.address = address;
-        this.salary = salary;
-    }
 }
--- pos-backend/src/main/java/lk/ijse/gdse66/pos_backend/dto/ItemDTO.java
+++ pos-backend/src/main/java/lk/ijse/gdse66/pos_backend/dto/ItemDTO.java
@@ -1,60 +1,18 @@
 package lk.ijse.gdse66.pos_backend.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class ItemDTO {
     private String code;
     private String description;
     private double unitPrice;
     private int qtyOnHand;
-
-    @Override
-    public String toString() {
-        return "ItemDTO{" +
-                "code='" + code + '\'' +
-                ", description='" + description + '\'' +
-                ", qtyOnHand=" + qtyOnHand +
-                ", unitPrice=" + unitPrice +
-                '}';
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public int getQtyOnHand() {
-        return qtyOnHand;
-    }
-
-    public void setQtyOnHand(int qtyOnHand) {
-        this.qtyOnHand = qtyOnHand;
-    }
-
-    public double getUnitPrice() {
-        return unitPrice;
-    }
-
-    public void setUnitPrice(double unitPrice) {
-        this.unitPrice = unitPrice;
-    }
-
-    public ItemDTO() {
-    }
-
-    public ItemDTO(String code, String description, double unitPrice, int qtyOnHand) {
-        this.code = code;
-        this.description = description;
-        this.qtyOnHand = qtyOnHand;
-        this.unitPrice = unitPrice;
-    }
 }
