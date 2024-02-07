539e6742f33ee8f6e8374ba76aaa17ca2b6b7ed6
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2024-02-07 11:22:30 +0530

    updated

--- pos-backend/src/main/java/lk/ijse/gdse66/pos_backend/api/ItemServlet.java
+++ pos-backend/src/main/java/lk/ijse/gdse66/pos_backend/api/ItemServlet.java
@@ -15,7 +15,6 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 import java.io.IOException;
-import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
