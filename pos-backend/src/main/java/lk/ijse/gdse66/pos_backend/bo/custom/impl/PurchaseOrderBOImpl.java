96cf4ae7d2f9acb055c944762fab8ff307035b2f
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2024-02-07 13:32:13 +0530

    order dao added

--- pos-backend/src/main/java/lk/ijse/gdse66/pos_backend/bo/custom/impl/PurchaseOrderBOImpl.java
+++ pos-backend/src/main/java/lk/ijse/gdse66/pos_backend/bo/custom/impl/PurchaseOrderBOImpl.java
@@ -9,6 +9,52 @@ import java.sql.SQLException;
 public class PurchaseOrderBOImpl implements PurchaseOrderBO {
     @Override
     public boolean saveOrder(OrderDTO dto, Connection connection) throws SQLException, ClassNotFoundException {
+        Connection connection = null;
+        try {
+            connection = DBConnection.getDbConnection().getConnection();
+            /*if order id already exist*/
+            if (orderDAO.exist(dto.getOrderId())) {
+                return false;
+            }
+
+            connection.setAutoCommit(false);
+
+            Orders orderEntity = new Orders(dto.getOrderId(), dto.getOrderDate(), dto.getCustomerId());
+            boolean orderAdded = orderDAO.save(orderEntity);
+            if (!orderAdded) {
+                connection.rollback();
+                connection.setAutoCommit(true);
+                return false;
+            }
+
+            for (OrderDetailDTO odDTO : dto.getOrderDetaisList()) {
+                OrderDetails orderDetailsEntity = new OrderDetails(odDTO.getOrderID(), odDTO.getItemCode(), odDTO.getQty(), odDTO.getUnitPrice());
+                boolean odAdded = orderDetailsDAO.save(orderDetailsEntity);
+                if (!odAdded) {
+                    connection.rollback();
+                    connection.setAutoCommit(true);
+                    return false;
+                }
+
+//                //Search & Update Item
+                ItemDTO item = findItemByID(orderDetailsEntity.getItemCode());
+                item.setQtyOnHand(item.getQtyOnHand() - orderDetailsEntity.getQty());
+                boolean itemUpdate = itemDAO.update(new Item(item.getCode(), item.getDescription(), item.getQtyOnHand(), item.getUnitPrice()));
+
+                if (!itemUpdate) {
+                    connection.rollback();
+                    connection.setAutoCommit(true);
+                    return false;
+                }
+            }
+            connection.commit();
+            connection.setAutoCommit(true);
+            return true;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
         return false;
     }
 }
---
+++ pos-backend/src/main/java/lk/ijse/gdse66/pos_backend/dao/custom/OrderDAO.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse66.pos_backend.dao.custom;
+
+import lk.ijse.gdse66.pos_backend.dao.CrudDAO;
+import lk.ijse.gdse66.pos_backend.entity.Orders;
+
+public interface OrderDAO extends CrudDAO<Orders,String> {
+}
