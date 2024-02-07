8085a1a2412d10e841933a23cc451e1a7cbfe5b8
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2024-02-07 15:25:52 +0530

    detail servlet added

---
+++ pos-backend/src/main/java/lk/ijse/gdse66/pos_backend/api/OrederDetailServlet.java
@@ -0,0 +1,24 @@
+package lk.ijse.gdse66.pos_backend.api;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+@WebServlet(urlPatterns = "/detail")
+public class OrederDetailServlet extends HttpServlet {
+
+    private DataSource source;
+
+    @Override
+    public void init() throws ServletException {
+        try {
+            source = (DataSource) new InitialContext().lookup("java:/comp/env/jdbc/pos");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+}
