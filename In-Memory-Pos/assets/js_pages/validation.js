4146ede2288587d8426237ea3da11fb620421996
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2024-02-08 15:25:03 +0530

    validation updated

--- In-Memory-Pos/assets/js_pages/validation.js
+++ In-Memory-Pos/assets/js_pages/validation.js
@@ -154,7 +154,7 @@ function setBtn() {
 //Items Validation
 
 
-const item_id_regx = /^(I00-)[0-9]{3}$/;
+const item_id_regx = /^(P0)[0-9]{3}$/;
 const description_regx = /^[A-Za-z ]{5,}$/;
 const price_regex = /^[0-9 ]{2,}$/;
 const qty_regex = /^[0-9 ]{2,}$/;
