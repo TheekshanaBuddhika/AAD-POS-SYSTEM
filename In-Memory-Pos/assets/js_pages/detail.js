eda99d7fd6eb5fa1c733ab63fa5d06b3ab7371a3
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2024-02-08 23:34:17 +0530

    order detail updated

--- In-Memory-Pos/assets/js_pages/detail.js
+++ In-Memory-Pos/assets/js_pages/detail.js
@@ -1,5 +1,25 @@
+loadAllOrderDetails();
+
 function loadAllOrderDetails(){
-    
+
+    $.ajax({
+            url: "http://localhost:8080/app/detail",
+            method: "GET",
+            success: function (resp) {
+                console.log("Success: ", resp);
+                for (const detail of resp) {
+                    $(`#order-detail-body`).append(`<tr>
+                    <td>${detail.orderId}</td>
+                    <td>${detail.itemCode}</td>
+                    <td>${detail.qty}</td>
+                    <td>${detail.unitPrice}</td>
+                   </tr>`);
+                }
+            },
+            error: function (error) {
+                console.log("Error: ", error);
+            }
+        });
+
 }
 
- loadAllOrderDetails();
